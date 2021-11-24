05884481918a5751125d16bf0ad9d8db6cf00e84
theblessedcity <theblessedcity@gmail.net>
2021-11-24 23:48:01 +0900

    test to use bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { SelectOTT } from "./pages/SelectOTT";
-import { SelectDate } from "./pages/SelectDate";
-import { SelectPay } from "./pages/SelectPay";
-import { SelectComplete } from "./pages/SelectComplete";
+import { SelectOTT } from './pages/SelectOTT';
+import { SelectDate } from './pages/SelectDate';
+import { SelectPay } from './pages/SelectPay';
+import { SelectComplete } from './pages/SelectComplete';
 
 const App = () => {
   return (
     <div className='App'>
       <Router>
         <Routes>
-          <Route path="/" exact element={<SelectOTT />} />
-          <Route path="/select-ott" exact element={<SelectOTT />} />
-          <Route path="/select-date" exact element={<SelectDate />} />
-          <Route path="/select-pay" exact element={<SelectPay />} />
-          <Route path="/select-complete" exact element={<SelectComplete />} />
+          <Route path='/' exact element={<SelectOTT />} />
+          <Route path='/select-ott' exact element={<SelectOTT />} />
+          <Route path='/select-date' exact element={<SelectDate />} />
+          <Route path='/select-pay' exact element={<SelectPay />} />
+          <Route path='/select-complete' exact element={<SelectComplete />} />
         </Routes>
       </Router>
     </div>
