3d1775e9841dda89e41256effaa4d948c4686294
theblessedcity <theblessedcity@gmail.net>
2021-11-24 23:48:01 +0900

    test to use bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import styled from 'styled-components';
+
+import * as BS from 'react-bootstrap';
+
+const Button = styled(BS.Button)`
+  border: 1px solid red;
+`;
+
 const App = () => {
-  return <div className='App'>Hello world</div>;
+  return (
+    <div className='App'>
+      <Button variant='secondary'>Hello world</Button>
+    </div>
+  );
 };
 
 export default App;
