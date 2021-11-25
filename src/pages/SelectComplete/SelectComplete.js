b9da6a77c7eefa3b6687eba4f4e42fd13b8e0abf
seonjeong <theblessedcity@hanmail.net>
2021-11-25 22:14:51 +0900

    add page - SelectPay

--- src/pages/SelectComplete/SelectComplete.js
+++ src/pages/SelectComplete/SelectComplete.js
@@ -1,5 +1,48 @@
+import { Container, Alert, Card, ButtonGroup } from 'react-bootstrap';
+
+import { Button } from '../../components/Button';
+
 const SelectComplete = () => {
-  return 'Hello SelectComplete';
-}
+  return (
+    <Container className='mt-5'>
+      <Alert variant='info'>
+        구매가 완료 되었습니다. 즐거운 시간 보내세요!
+      </Alert>
+      <Card>
+        <Card.Body>
+          <Card.Title>신청한 서비스</Card.Title>
+          <Card.Text>넷플릭스</Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>이용 기간</Card.Title>
+          <Card.Text>
+            0000년 00월 00일 AM 6:00 ~ 0000년 00월 00일 AM 6:00
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>결제 금액</Card.Title>
+          <Card.Text>2000원(총 1000원 할인 받았어요)</Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>계정 정보 보러가기</Card.Title>
+          <Card.Text>
+            <Button variant='outline-primary' size='sm'>
+              마이 페이지로 이동
+            </Button>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <ButtonGroup className='mt-3'>
+        <Button>계정 정보 카톡으로 전송</Button>
+      </ButtonGroup>
+    </Container>
+  );
+};
 
 export default SelectComplete;
--- src/pages/SelectPay/SelectPay.js
+++ src/pages/SelectPay/SelectPay.js
@@ -1,5 +1,105 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import {
+  Container,
+  Alert,
+  Card,
+  ToggleButton,
+  Form,
+  ButtonGroup,
+} from 'react-bootstrap';
+
+import { Button } from '../../components/Button';
+
 const SelectPay = () => {
-  return 'Hello SelectPay';
-}
+  const navigate = useNavigate();
+
+  const [selectedPay, setSelectedPay] = useState('pay-registered');
+  return (
+    <Container className='mt-5'>
+      <Alert variant='info'>결제 수단을 선택해주세요</Alert>
+      <Card>
+        <Card.Body>
+          <Card.Title>구매 상품</Card.Title>
+          <Card.Text>넷플릭스 3일 이용권(AM 6:00 부터)</Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>상품가격</Card.Title>
+          <Card.Text style={{ textDecoration: 'line-through' }}>
+            3000원
+          </Card.Text>
+          <Card.Text>2000원</Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>결제 수단</Card.Title>
+          <Card.Text>
+            <ToggleButton
+              id='pay-registered'
+              type='radio'
+              name='pay'
+              value='pay-registered'
+              checked={'pay-registered' === selectedPay}
+            >
+              등록된 결제 수단
+            </ToggleButton>
+            <ToggleButton
+              id='pay-credit'
+              type='radio'
+              name='pay'
+              value='pay-credit'
+              checked={'pay-credit' === selectedPay}
+            >
+              신용카드
+            </ToggleButton>
+            <ToggleButton
+              id='pay-phone'
+              type='radio'
+              name='pay'
+              value='pay-phone'
+              checked={'pay-phone' === selectedPay}
+            >
+              휴대폰 결제
+            </ToggleButton>
+            <ToggleButton
+              id='pay-naver'
+              type='radio'
+              name='pay'
+              value='pay-naver'
+              checked={'pay-naver' === selectedPay}
+            >
+              네이버 페이
+            </ToggleButton>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Text>
+            <Form.Check
+              id='accept-term'
+              name='accept-term'
+              label='이용 약관 동의'
+            />
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <ButtonGroup className='mt-3'>
+        <Button
+          onClick={() => {
+            navigate({
+              pathname: '/select-complete',
+            });
+          }}
+        >
+          구매하기
+        </Button>
+      </ButtonGroup>
+    </Container>
+  );
+};
 
 export default SelectPay;
