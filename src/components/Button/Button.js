8c69318207f5b7b4765efc845b64618a5da411aa
seonjeong <theblessedcity@hanmail.net>
2021-11-25 22:33:04 +0900

    change button design

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,6 +1,12 @@
 import { Button } from 'react-bootstrap';
 import styled from 'styled-components';
 
-const Default = styled(Button)``;
+const Default = styled(Button)`
+  border-radius: 30px;
+  width: 100%;
+  &:hover {
+    box-shadow: 0 5px #333;
+  }
+`;
 
 export default Default;
