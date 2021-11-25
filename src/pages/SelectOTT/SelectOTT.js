445331564014a9f22afb54f1a0a4112498012d43
seonjeong <theblessedcity@hanmail.net>
2021-11-25 21:19:38 +0900

    useState for ott servie arr in select-ott

--- src/pages/SelectOTT/SelectOTT.js
+++ src/pages/SelectOTT/SelectOTT.js
@@ -1,50 +1,43 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import OTTCard from './SelectOTT.style';
-import { Button } from '../../components/Button';
 import { Container, Row, Col } from 'react-bootstrap';
+
+import { Button } from '../../components/Button';
+import OTTCard from './SelectOTT.style';
 
 const SelectOTT = () => {
   const navigate = useNavigate();
+
+  const [otts, setOtts] = useState({
+    netflix: { id: 'netflix', title: '넷플릭스' },
+    youtube: { id: 'youtube', title: '유투브' },
+  });
   return (
     <Container>
       <Row>
-        <Col>
-          <OTTCard>
-            <OTTCard.Body>
-              <OTTCard.Title>Netflix</OTTCard.Title>
-              <Button
-                variant='primary'
-                onClick={() => {
-                  navigate({
-                    pathname: 'select-date',
-                    search: 'ott=netflix',
-                  });
-                }}
-              >
-                Go
-              </Button>
-            </OTTCard.Body>
-          </OTTCard>
-        </Col>
-        <Col>
-          <OTTCard>
-            <OTTCard.Body>
-              <OTTCard.Title>Youtube</OTTCard.Title>
-              <Button
-                variant='primary'
-                onClick={() => {
-                  navigate({
-                    pathname: 'select-date',
-                    search: 'ott=youtoube',
-                  });
-                }}
-              >
-                Go
-              </Button>
-            </OTTCard.Body>
-          </OTTCard>
-        </Col>
+        {Object.entries(otts).map(([ott, ottData]) => {
+          return (
+            <Col>
+              <OTTCard>
+                <OTTCard.Body>
+                  <OTTCard.Title>{ottData.title}</OTTCard.Title>
+                  <Button
+                    variant='primary'
+                    onClick={() => {
+                      navigate({
+                        pathname: 'select-date',
+                        search: `ott=${ott}`,
+                      });
+                    }}
+                  >
+                    Go
+                  </Button>
+                </OTTCard.Body>
+              </OTTCard>
+            </Col>
+          );
+        })}
       </Row>
     </Container>
   );
