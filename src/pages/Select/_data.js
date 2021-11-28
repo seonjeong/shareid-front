642b4356819aa9c3b055a271a21a70bc5459da92
seonjeong <theblessedcity@hanmail.net>
2021-11-28 23:58:11 +0900

    add data about ott service

---
+++ src/pages/Select/_data.js
@@ -0,0 +1,18 @@
+export const ottService = {
+  netflix: {
+    id: 'netflix',
+    title: '넷플릭스',
+    pays: {
+      day1: { pay: 1000, sale: 0 },
+      day3: { pay: 3000, sale: 1000 },
+    },
+  },
+  youtube: {
+    id: 'youtube',
+    title: '유투브',
+    pays: {
+      day1: { pay: 1000, sale: 0 },
+      day3: { pay: 3000, sale: 1000 },
+    },
+  },
+};
